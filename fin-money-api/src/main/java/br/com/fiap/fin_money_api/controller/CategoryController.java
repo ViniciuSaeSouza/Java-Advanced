550203a02a5d4c4a4af3df97374520c6e721eaea
Vinicius Saes <viniciusaesouza@gmail.com>
2025-02-24 11:35:18 -0300

    feat: RequestMapping notation

--- fin-money-api/src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
+++ fin-money-api/src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
@@ -1,9 +1,12 @@
 package br.com.fiap.fin_money_api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+	
+	@RequestMapping
 	public String index(){
 		return "ok";
 	}	
