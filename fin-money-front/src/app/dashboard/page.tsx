5aa9c1ccc1b46400ff5bff9285c9787252d775a2
Vinicius Saes <viniciusaesouza@gmail.com>
2025-03-20 11:58:40 -0300

    feat: navbar component

--- fin-money-front/src/app/dashboard/page.tsx
+++ fin-money-front/src/app/dashboard/page.tsx
@@ -1,12 +1,13 @@
 import NavBar from "@/components/nav-bar";
+import React from "react";
 
-export default function CategoriesPage() {
+export default function DashboardPage() {
   return (
     <>
-    <NavBar active="categorias"/>
+      <NavBar active="dashboard"/>
       <main className="flex items-center justify-center">
         <div className="bg-slate-800 rounded min-w-2/3 p-6 m-6">
-          <h2 className="text-lg font-bold">Categorias</h2>
+          <h2 className="text-lg font-bold">DashBoard</h2>
         </div>
       </main>
     </>
